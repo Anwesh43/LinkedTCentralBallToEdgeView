06f6d476af60f1828e0042fa70992517fa98f2b7
Anwesh43 <anweshthecool0@gmail.com>
2020-08-27 01:56:12 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtcentralballtoedgeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtcentralballtoedgeview/MainActivity.kt
@@ -2,12 +2,19 @@ package com.anwesh.uiprojects.linkedtcentralballtoedgeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.tcenterballtoedgeview.TCenterBallToEdgeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TCenterBallToEdgeView.create(this)
+        fullScreen()
     }
 }
 
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
