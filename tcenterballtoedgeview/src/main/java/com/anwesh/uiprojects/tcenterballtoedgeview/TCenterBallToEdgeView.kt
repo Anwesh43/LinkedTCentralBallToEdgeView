35e9a28277baceccea3e6204932591f7ad404576
Anwesh43 <anweshthecool0@gmail.com>
2020-08-27 01:41:32 +0530

    adding TCenterBallToEdgeView class

--- tcenterballtoedgeview/src/main/java/com/anwesh/uiprojects/tcenterballtoedgeview/TCenterBallToEdgeView.kt
+++ tcenterballtoedgeview/src/main/java/com/anwesh/uiprojects/tcenterballtoedgeview/TCenterBallToEdgeView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawTCBENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTCenterBallToEdge(scale, w, h, paint)
 }
+
+class TCenterBallToEdgeView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
